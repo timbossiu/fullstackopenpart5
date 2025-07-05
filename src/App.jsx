b181925f3da41b54a36e5c31bde1a0541eba6202
Tim Alderkot <timalderkot@gmail.com>
2025-07-05 13:20:36 +0200

    done with 5.2

--- src/App.jsx
+++ src/App.jsx
@@ -33,6 +33,11 @@ const App = () => {
     }
   }
 
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+  }
+
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
@@ -80,7 +85,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in<button>logout</button></p>
+      <p>{user.name} logged in<button onClick={() => handleLogout()}>logout</button></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
