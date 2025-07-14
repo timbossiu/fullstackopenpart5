8e761f8f3105ee412fbc2c2078b70f4cb77f8ab2
Tim Alderkot <timalderkot@gmail.com>
2025-07-14 20:46:07 +0200

    started with 5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,6 +1,9 @@
+import Togglable from "./Togglable"
+
 const Blog = ({ blog }) => (
   <div>
     {blog.title} {blog.author}
+    <Togglable buttonLabel="view" buttonLabelHide="hide"></Togglable>
   </div>  
 )
 
