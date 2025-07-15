1b4ff49b68842e38dba4524fab2949e1b7490cc3
Tim Alderkot <timalderkot@gmail.com>
2025-07-15 20:55:37 +0200

    5.7 finished

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,10 +1,35 @@
-import Togglable from "./Togglable"
+import { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-    <Togglable buttonLabel="view" buttonLabelHide="hide"></Togglable>
-  </div>  
-)
+const Blog = ({ blog }) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const addLike = (likes) => {
+    likes += 1
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} <button onClick={() => setVisible(true)}>view</button> 
+      </div>
+      <div style={showWhenVisible}>
+        <p>{blog.url}</p>
+        <p>likes {blog.likes} <button >like</button></p>
+        <button onClick={() => setVisible(false)}>hide</button>
+        <p>{blog.user ? blog.user.name : 'no user'}</p>
+      </div> 
+    </div> 
+  )}
 
 export default Blog
