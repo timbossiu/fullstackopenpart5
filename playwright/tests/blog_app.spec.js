c568fab80a04449f2c2231b43d0562e848f53fde
Tim Alderkot <timalderkot@gmail.com>
2025-08-10 21:24:48 +0200

    done with 5.17

---
+++ playwright/tests/blog_app.spec.js
@@ -0,0 +1,14 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+    await expect(page.getByText('username')).toBeVisible()
+    await expect(page.getByText('password')).toBeVisible()
+    await expect(page.getByText('log in to application')).toBeVisible()
+  })
+})
